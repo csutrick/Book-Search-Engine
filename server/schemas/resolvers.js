ab68148b32bbdd6e7df878dd5ba7b8b055b16aa0
Cole Sutrick <papacole@Coles-MacBook-Pro-4.local>
2023-08-03 13:21:49 -0500

    Added schemas folder with files

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,11 @@
+const {} = require('../models');
+
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {},
+  Mutation: {},
+};
+
+module.exports = resolvers;
